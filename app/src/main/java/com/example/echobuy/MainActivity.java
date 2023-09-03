5779e0f3fcfd8f0008407cceb5b527668a293e37
soumyakant <soumykantarout@gmail.com>
2023-09-03 20:10:37 +0530

    on working

--- app/src/main/java/com/example/echobuy/MainActivity.java
+++ app/src/main/java/com/example/echobuy/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.echobuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button1, button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+
+        Button button1 =(Button) findViewById (R.id.signin);
+        Button button2 = (Button) findViewById (R.id.signup);
+
+        button1.setOnClickListener (new View.OnClickListener ( ) {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (getApplicationContext (), loginactivity.class);
+                startActivity (intent);
+            }
+        });
+
+        button2.setOnClickListener (new View.OnClickListener ( ) {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (getApplicationContext (), Signup_activity.class);
+                startActivity (intent);
+            }
+        });
     }
 }
