2d2fc06382ffbb689a1dadacd8b651575852917b
soumyakant <soumykantarout@gmail.com>
2023-09-26 00:12:58 +0530

    on working

--- app/src/main/java/com/example/echobuy/Signup_activity.java
+++ app/src/main/java/com/example/echobuy/Signup_activity.java
@@ -1,15 +1,19 @@
 package com.example.echobuy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Signup_activity extends AppCompatActivity {
 
     TextView textView;
+    private VideoView videoViewone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
@@ -17,6 +21,17 @@ public class Signup_activity extends AppCompatActivity {
 
         TextView textView = (TextView)findViewById (R.id.already);
 
+        videoViewone = findViewById (R.id.videoViewone);
+
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.sign; // Replace 'your_video' with the name of your video file in res/raw
+        Uri uri = Uri.parse(videoPath);
+        videoViewone.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoViewone);
+        videoViewone.setMediaController(mediaController);
+
+        videoViewone.start();
         textView.setOnClickListener (new View.OnClickListener ( ) {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/echobuy/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/echobuy/ui/home/HomeFragment.java
@@ -1,19 +1,23 @@
 package com.example.echobuy.ui.home;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.echobuy.R;
 import com.example.echobuy.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private VideoView videoView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,8 +27,13 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate (inflater, container, false);
         View root = binding.getRoot ( );
 
-       // final TextView textView = binding.textHome;
-       // homeViewModel.getText ( ).observe (getViewLifecycleOwner ( ), textView::setText);
+        videoView = root.findViewById (R.id.videoView);
+
+        Uri videoUri = Uri.parse("android.resource://" + requireActivity().getPackageName() + "/" + R.raw.shop);
+        videoView.setVideoURI(videoUri);
+        videoView.start();
+
+
         return root;
     }
 
